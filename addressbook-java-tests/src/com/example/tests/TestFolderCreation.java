f2e1f98029a23253e99b531ff911bbbbeccf0e47
zhannaki <moe_pismo@bk.ru>
2015-11-25 17:42:01 +0300

    Task16
    Домашнее задание 16. Разработать тесты для проверки создания папок с
    одинаковыми именами

--- addressbook-java-tests/src/com/example/tests/TestFolderCreation.java
+++ addressbook-java-tests/src/com/example/tests/TestFolderCreation.java
@@ -4,15 +4,36 @@ import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
+
 public class TestFolderCreation extends TestBase {
 
 	@Test
 	public void testFoldersCreation(){
-		Folders oldFolders =  app.getFolderHelper().getFolders();
-		String folder = "newfolder11";		
-		assertThat(app.getFolderHelper().createFolder(folder), is(nullValue()));
+		addNewFolder("newfolder11");
+		addNewFolder("newfolder112");
+	}
+
+	@Test
+	public void testCreate2FoldersErrorExpected(){
+		String folder = "newfolder1";
+		addNewFolder(folder);
+		addDuplicatedFolder(folder);
+	}
+	
+	private void addNewFolder(String folder) {
+		Folders oldFolders =  app.getFolderHelper().getFolders();						
+		assertThat(app.getFolderHelper().createFolder(folder),is(nullValue()));			
 		Folders newFolders =  app.getFolderHelper().getFolders();
-		assertThat(newFolders, equalTo(oldFolders.withAdded(folder)));
+		assertThat(newFolders, equalTo(oldFolders.withAdded(folder)));			
+	}
+	
+	private void addDuplicatedFolder(String folder) {
+		Folders oldFolders =  app.getFolderHelper().getFolders();						
+		assertThat(app.getFolderHelper().createFolder(folder),containsString("Duplicated folder name"));			
+		Folders newFolders =  app.getFolderHelper().getFolders();
+		assertThat(newFolders, equalTo(oldFolders));			
 	}
 	
 }
